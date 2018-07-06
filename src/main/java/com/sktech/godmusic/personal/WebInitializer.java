c312b190efd5479c2b86984a42ee059e5a94f58c
안영현(Younghyun Ahn)/Syrup개발팀/SKP <younghyun.ahn@sk.com>
2018-07-06 15:21:53 +0900

    initial

---
+++ src/main/java/com/sktech/godmusic/personal/WebInitializer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 SK TECHX.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of SK TECHX.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with SK TECHX.
+ */
+
+package com.sktech.godmusic.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * 설명 : XXXXXXXXX
+ *
+ * @author 안영현(Younghyun Ahn)
+ * @date 2018.06.30
+ */
+
+@SpringBootApplication
+public class WebInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // set register error pagefilter false
+        setRegisterErrorPageFilter(false);
+        return application.sources(WebInitializer.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebInitializer.class, args);
+    }
+}
+
