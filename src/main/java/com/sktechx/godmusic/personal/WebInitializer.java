96a40f19482312cae046e9ca764f580151c64df7
안영현(Younghyun Ahn)/Syrup개발팀/SKP <younghyun.ahn@sk.com>
2018-07-09 15:32:19 +0900

    initial

--- src/main/java/com/sktechx/godmusic/personal/WebInitializer.java
+++ src/main/java/com/sktechx/godmusic/personal/WebInitializer.java
@@ -14,6 +14,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 설명 : XXXXXXXXX
@@ -23,6 +24,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  */
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WebInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
