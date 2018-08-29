e410288b414afa8562524643a7e3c8a63e4bba78
김관효(Bob) <kwanhyo.kim@sk.com>
2018-08-29 16:26:04 +0900

    qa hot fix :
    /personal/v1/channels/recentListened
    /personal/v1/tracks/recentlistened
    
    캐릭터 번호 하드코딩 제거

--- src/main/java/com/sktechx/godmusic/personal/rest/controller/v1/ChannelController.java
+++ src/main/java/com/sktechx/godmusic/personal/rest/controller/v1/ChannelController.java
@@ -1,5 +1,17 @@
 package com.sktechx.godmusic.personal.rest.controller.v1;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.sktechx.godmusic.lib.domain.CommonApiResponse;
 import com.sktechx.godmusic.lib.domain.GMContext;
 import com.sktechx.godmusic.lib.domain.RequestGMContext;
@@ -13,18 +25,7 @@ import com.sktechx.godmusic.personal.rest.service.ChannelService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
-
-import java.time.LocalDate;
-import java.util.List;
 
 /**
  * 설명 : XXXXXXXXX
@@ -47,10 +48,10 @@ public class ChannelController {
     public CommonApiResponse<ListResponse> getLastListenHistory(
             @ApiIgnore @RequestGMContext GMContext ctx, @PageableDefault(size=100, page=0) Pageable pageable){
 
-        long characterNo = 1000284;
+//        long characterNo = 1000284;
         DayType dayType = DayType.findDayOfWeek(LocalDate.now().getDayOfWeek());
 
-        List<LastListenHistoryDto> lastListenHistory = channelService.getLastListenHistory(characterNo, dayType, ctx.getOsType());
+        List<LastListenHistoryDto> lastListenHistory = channelService.getLastListenHistory(ctx.getCharacterNo(), dayType, ctx.getOsType());
         if(CollectionUtils.isEmpty(lastListenHistory)) throw new CommonBusinessException(CommonErrorMessage.EMPTY_DATA);
 
         int start = pageable.getPageNumber() * pageable.getPageSize();
--- src/main/java/com/sktechx/godmusic/personal/rest/controller/v1/TrackController.java
+++ src/main/java/com/sktechx/godmusic/personal/rest/controller/v1/TrackController.java
@@ -10,19 +10,24 @@
 
 package com.sktechx.godmusic.personal.rest.controller.v1;
 
-import com.sktechx.godmusic.lib.domain.CommonApiResponse;
-import com.sktechx.godmusic.lib.domain.GMContext;
-import com.sktechx.godmusic.personal.common.domain.ListResponse;
-import com.sktechx.godmusic.personal.common.domain.domain.Naming;
-import com.sktechx.godmusic.personal.rest.service.TrackService;
-import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sktechx.godmusic.lib.domain.CommonApiResponse;
+import com.sktechx.godmusic.lib.domain.GMContext;
+import com.sktechx.godmusic.lib.domain.RequestGMContext;
+import com.sktechx.godmusic.personal.common.domain.ListResponse;
+import com.sktechx.godmusic.personal.common.domain.domain.Naming;
+import com.sktechx.godmusic.personal.rest.service.TrackService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Slf4j
 @RestController
@@ -39,10 +44,19 @@ public class TrackController {
         return new CommonApiResponse<>(new ListResponse(trackService.mostTrackList(GMContext.getContext().getCharacterNo(), pageable)));
     }
 
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", dataType = "integer", paramType = "query",
+                    value = "Results page you want to retrieve (0..N)", defaultValue = "0"),
+            @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query",
+                    value = "Number of records per page.", defaultValue = "5")
+    })
     @ApiOperation(value = "최근 들은 by Peter ( 기존 /v2/my/track/recent/list GET )")
     @GetMapping("/recentlistened")
-    public CommonApiResponse<ListResponse> recentListenedTrackList(@PageableDefault(size=300, page=0) Pageable pageable) {
-        long characterNo = 1234567;
-        return new CommonApiResponse<>(new ListResponse(trackService.getMyRecentTrackList(characterNo, pageable)));
+    public CommonApiResponse<ListResponse> recentListenedTrackList(
+            @ApiIgnore @RequestGMContext GMContext ctx,
+            @ApiIgnore @PageableDefault(size=300, page=0) Pageable pageable) {
+//        long characterNo = 1234567;
+        return new CommonApiResponse<>(new ListResponse(trackService.getMyRecentTrackList(ctx.getCharacterNo(), pageable)));
     }
 }
