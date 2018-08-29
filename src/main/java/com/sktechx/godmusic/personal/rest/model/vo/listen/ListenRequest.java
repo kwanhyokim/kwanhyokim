3ac09208755b7923d8690024aa869b0cfc0b59dc
안영현(Lake) <younghyun.ahn@sk.com>
2018-08-29 15:02:21 +0900

    @Length -> @Max

--- src/main/java/com/sktechx/godmusic/personal/rest/model/vo/listen/ListenRequest.java
+++ src/main/java/com/sktechx/godmusic/personal/rest/model/vo/listen/ListenRequest.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -15,14 +16,15 @@ import javax.validation.constraints.NotNull;
  */
 @Data
 public class ListenRequest {
-	@Length(max = 10)
+//	@Length(max = 10)
 	@NotNull
 	@ApiModelProperty(name = "listenType", value = "청취 타입(CHNL: 일반채널(DJ), ALBUM: 앨범, MY_CHNL: 마이채널, CHART: 차트, " +
 			"RC_ATST_TR: 추천(선호/유사 아티스트 인기곡), RC_SML_TR: 추천(유사곡), RC_GR_TR: 추천(선호 장르 유사곡), RC_CF_TR: 추천(CF 추천 알고리즘)",
 			allowableValues = "CHNL, ALBUM, MY_CHNL, CHART, RC_ATST_TR, RC_SML_TR, RC_GR_TR, RC_CF_TR")
 	private String listenType;
 
-	@Length(max = 20)
+//	@Length(max = 20)
+	@Max(20)
 	@NotNull
 	@ApiModelProperty(name = "listenTypeId", value = "좋아하는 타입에 맞는 ID의 리스트")
 	private Long listenTypeId;
