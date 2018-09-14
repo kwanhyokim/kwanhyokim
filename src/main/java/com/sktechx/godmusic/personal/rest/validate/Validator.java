20bc3af78f444297951f282ed7d023ce31fd0ce2
최훈영(Kobe) <hunyoung.choi@sk.com>
2018-09-14 20:07:53 +0900

    회원번호 없을때 내려가는 에러 수정

--- src/main/java/com/sktechx/godmusic/personal/rest/validate/Validator.java
+++ src/main/java/com/sktechx/godmusic/personal/rest/validate/Validator.java
@@ -3,6 +3,7 @@ package com.sktechx.godmusic.personal.rest.validate;
 import com.sktechx.godmusic.lib.domain.GMContext;
 import com.sktechx.godmusic.lib.domain.exception.CommonErrorDomain;
 import com.sktechx.godmusic.lib.domain.exception.LoginValidationException;
+import com.sktechx.godmusic.personal.common.exception.PersonalErrorDomain;
 import org.springframework.util.ObjectUtils;
 
 /**
@@ -19,9 +20,9 @@ public class Validator {
 
 	public static void loginValidate(GMContext context) {
 		try {
-			if(ObjectUtils.isEmpty(context.getMemberNo()) || ObjectUtils.isEmpty(context.getCharacterNo())) throw new LoginValidationException(CommonErrorDomain.UNAUTHORIZED);
+			if(ObjectUtils.isEmpty(context.getMemberNo()) || ObjectUtils.isEmpty(context.getCharacterNo())) throw new LoginValidationException(PersonalErrorDomain.GM_CONTEXT_MEMBER_NO_NOT_EXIST);
 		} catch (NullPointerException e) {
-			throw new LoginValidationException(CommonErrorDomain.UNAUTHORIZED);
+			throw new LoginValidationException(PersonalErrorDomain.GM_CONTEXT_MEMBER_NO_NOT_EXIST);
 		}
 	}
 
@@ -29,8 +30,8 @@ public class Validator {
 
 		GMContext currentContext = GMContext.getContext();
 
-        if(ObjectUtils.isEmpty(currentContext.getMemberNo())) throw new LoginValidationException(CommonErrorDomain.UNAUTHORIZED);
-        if(!ObjectUtils.isEmpty(memberNo) && !memberNo.equals(currentContext.getMemberNo())) throw new LoginValidationException(CommonErrorDomain.UNAUTHORIZED);
-        if(!ObjectUtils.isEmpty(characterNo) && !characterNo.equals(currentContext.getCharacterNo())) throw new LoginValidationException(CommonErrorDomain.UNAUTHORIZED);
+        if(ObjectUtils.isEmpty(currentContext.getMemberNo())) throw new LoginValidationException(PersonalErrorDomain.GM_CONTEXT_MEMBER_NO_NOT_EXIST);
+        if(!ObjectUtils.isEmpty(memberNo) && !memberNo.equals(currentContext.getMemberNo())) throw new LoginValidationException(PersonalErrorDomain.GM_CONTEXT_MEMBER_NO_NOT_EXIST);
+        if(!ObjectUtils.isEmpty(characterNo) && !characterNo.equals(currentContext.getCharacterNo())) throw new LoginValidationException(PersonalErrorDomain.GM_CONTEXT_MEMBER_NO_NOT_EXIST);
 	}
 }
