c40e21a722244587c085ed06170f5ab6d9e45231
안영현(Lake) <younghyun.ahn@sk.com>
2018-09-18 13:31:26 +0900

    문구변경: my플레이리스트 -> 내 리스트

--- src/main/java/com/sktechx/godmusic/personal/common/exception/PersonalErrorDomain.java
+++ src/main/java/com/sktechx/godmusic/personal/common/exception/PersonalErrorDomain.java
@@ -26,9 +26,9 @@ public enum PersonalErrorDomain implements ErrorDomain {
 	, ALBUM_OVER_ADD_LIKE(4090709, HttpStatus.CONFLICT, "좋아요 한 앨범을 더 이상 추가할 수 없습니다. 최대 1000개까지 담을 수 있습니다." ,"좋아하는 앨범 최대값 초과")
 	, CHART_DUPLICATED_LIKE(4090710, HttpStatus.CONFLICT, "이미 추가된 차트 입니다." ,"이미 추가된 차트")
 	, CHART_OVER_ADD_LIKE(4090711, HttpStatus.CONFLICT, "좋아요 한 차트을 더 이상 추가할 수 없습니다. 최대 1000개까지 담을 수 있습니다." ,"좋아하는 차트 최대값 초과")
-	, MY_CHANNEL_OVER_CREATE(4090712, HttpStatus.CONFLICT,"my플레이리스트를 더 이상 추가할 수 없습니다.\n최대 1000개의 my플레이리스트를 만들 수 있습니다.","My 플레이 리스트 생성 최대값 초과")
-	, MY_CHANNEL_TRACK_OVER_ADD(4090713, HttpStatus.CONFLICT, "my플레이리스트에는 최대 1000곡까지 담을 수 있습니다.\n새로운 플레이리스트를 만들어 주세요.","My 플레이 리스트 곡 추가 최대값 초과")
-	, MY_CHANNEL_DUPLICATED_NAME(4090714, HttpStatus.CONFLICT, "동일한 my플레이리스트 명이 이미 존재합니다" ,"채널명 중복")
+	, MY_CHANNEL_OVER_CREATE(4090712, HttpStatus.CONFLICT,"내 리스트를 더 이상 추가할 수 없습니다.\n최대 1000개의 내 리스트를 만들 수 있습니다.","My 플레이 리스트 생성 최대값 초과")
+	, MY_CHANNEL_TRACK_OVER_ADD(4090713, HttpStatus.CONFLICT, "내 리스트에는 최대 1000곡까지 담을 수 있습니다.\n새로운 플레이리스트를 만들어 주세요.","My 플레이 리스트 곡 추가 최대값 초과")
+	, MY_CHANNEL_DUPLICATED_NAME(4090714, HttpStatus.CONFLICT, "동일한 내 리스트 명이 이미 존재합니다" ,"채널명 중복")
 //	, MY_CHANNEL_NOT_FOUND(4090701, HttpStatus.BAD_REQUEST, "요청하신 채널이 존재하지 않습니다.", "요청 채널 에러")
 	// My 		0300
 	, CHANNEL_NOT_FOUND(4090715, HttpStatus.BAD_REQUEST, "존재 하지 않은 채널 정보 입니다.", "채널 정보 없음")
