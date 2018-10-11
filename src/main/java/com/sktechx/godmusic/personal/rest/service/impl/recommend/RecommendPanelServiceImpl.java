6bd6331de64960ee24a7ab6ddfe7e71fab49cd85
김관효(Bob) <kwanhyo.kim@sk.com>
2018-10-11 14:49:15 +0900

    [GODMUSIC-2501] 추천 상세 subtitle 아티스트 수 최대 5명으로 조정

--- src/main/java/com/sktechx/godmusic/personal/rest/service/impl/recommend/RecommendPanelServiceImpl.java
+++ src/main/java/com/sktechx/godmusic/personal/rest/service/impl/recommend/RecommendPanelServiceImpl.java
@@ -243,7 +243,7 @@ public class RecommendPanelServiceImpl implements RecommendPanelService {
 
                 List<String> artistNameList = new ArrayList<>();
 
-                artistDtoList.forEach(artistDto -> artistNameList.add(artistDto.getArtistName()));
+                artistDtoList.stream().limit(5).forEach(artistDto -> artistNameList.add(artistDto.getArtistName()));
                 // 아티스트의 첫 이미지를 배경 이미지로 사용
                 panel =  new RecommendPanelInfoDto.Builder()
                         .title(RecommendConstant.ARTIST_PANEL_TITLE)
