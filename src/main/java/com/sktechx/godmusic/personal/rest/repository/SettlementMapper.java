a6446ba83c0bb7547d0a212d50b20c825d1ac7c9
Sanghyun Park, 1100493 <sanghyun.park.tx@sk.com>
2019-03-13 09:43:40 +0900

    GODMUSIC-5215 MCP 서비스 코드 적용

---
+++ src/main/java/com/sktechx/godmusic/personal/rest/repository/SettlementMapper.java
@@ -0,0 +1,15 @@
+package com.sktechx.godmusic.personal.rest.repository;
+
+import com.sktechx.godmusic.lib.mybatis.annotation.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * 정산 코드 처리를 위한 mapper
+ *
+ * @author 박상현/SKTECH (sanghyun.park.tx@sk.com)
+ * @date 2019. 3. 11.
+ */
+@BaseMapper
+public interface SettlementMapper {
+	String selectServiceCode(@Param("memberNo")Long memberNo, @Param("playType")String playType);
+}
