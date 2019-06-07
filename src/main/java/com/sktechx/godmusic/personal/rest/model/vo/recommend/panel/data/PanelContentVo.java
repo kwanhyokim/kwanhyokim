57ba4793ff8db37f2230c59bbc5388a4f22da1e9
김관효(Bob) <kwanhyo.kim@sk.com>
2019-06-07 13:21:29 +0900

    [GODMUSIC-8000][FLO] (WEB) FLO 추천패널 재생목록에 추가 시 플레이리스트명에 날짜가 노출되지 않는 문제

--- src/main/java/com/sktechx/godmusic/personal/rest/model/vo/recommend/panel/data/PanelContentVo.java
+++ src/main/java/com/sktechx/godmusic/personal/rest/model/vo/recommend/panel/data/PanelContentVo.java
@@ -10,17 +10,21 @@
 
 package com.sktechx.godmusic.personal.rest.model.vo.recommend.panel.data;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.util.ObjectUtils;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sktechx.godmusic.lib.domain.code.OsType;
 import com.sktechx.godmusic.lib.domain.code.YnType;
 import com.sktechx.godmusic.lib.mybatis.code.CodeEnum;
 import com.sktechx.godmusic.personal.rest.model.dto.ArtistDto;
 import com.sktechx.godmusic.personal.rest.model.dto.TrackDto;
 import lombok.Data;
-
-import java.util.Date;
-import java.util.List;
 
 /**
  * 설명 : 추천 패널에서 사용할 공통 컨텐츠
@@ -51,4 +55,23 @@ public class PanelContentVo {
     private YnType renewYn;
 
     private GenreVo genre;
+
+    @JsonIgnore
+    private OsType osType;
+
+    // WEB 에서 추천 아이디가 너무 길 경우 Number 타입은 정확하게 변환이 안되어 web 요청일 경우 String 타입으로 내려줌
+    public Object getId(){
+        if(ObjectUtils.isEmpty(osType) || !OsType.WEB.equals(osType)){
+            return id;
+        }else{
+            return String.valueOf(id);
+        }
+    }
+
+    // WEB 요청으로 응답값 추가
+    @JsonProperty("renewDateTime")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone="Asia/Seoul")
+    public Date getRenewDtime(){
+        return new Date();
+    }
 }
