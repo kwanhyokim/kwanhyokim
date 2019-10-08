8ee13c9cff53d036c3f719d4bd9b939b56fd5dcf
Bob <kwanhyo.kim@iriver.com>
2019-10-08 17:03:03 +0900

    [GODMUSIC-11568] [server] 동영상 홈 - 좋아하는 아티스트 최신 영상 API : 단말 위해 캐릭터 번호 없어도 임시로 데이터 내려주게 수정

--- src/main/java/com/sktechx/godmusic/personal/rest/controller/v1/PreferenceController.java
+++ src/main/java/com/sktechx/godmusic/personal/rest/controller/v1/PreferenceController.java
@@ -16,7 +16,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 import com.sktechx.godmusic.lib.domain.CommonApiResponse;
