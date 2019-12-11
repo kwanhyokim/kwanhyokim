1f720a3d2b360e1dd26563ef2b4820bd72e63a77
조민국/Music사업TF/SKP <skp.9261059@sk.com>
2019-12-11 17:07:14 +0900

    GODMUSIC-12756 빅스비 청취로그 OsType TIZEN 추가

--- src/main/java/com/sktechx/godmusic/personal/common/domain/type/OsType.java
+++ src/main/java/com/sktechx/godmusic/personal/common/domain/type/OsType.java
@@ -1,10 +1,9 @@
 package com.sktechx.godmusic.personal.common.domain.type;
 
-import org.apache.ibatis.type.MappedTypes;
-
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.sktechx.godmusic.lib.mybatis.code.CodeEnum;
 import com.sktechx.godmusic.lib.mybatis.code.CodeEnumTypeHandler;
+import org.apache.ibatis.type.MappedTypes;
 
 /**
  * Created by Kobe.
@@ -17,7 +16,8 @@ public enum OsType implements CodeEnum {
 	ALL("ALL","ALL", "ALL"),
 	AOS("AOS", "A","Android"),
 	IOS("IOS","I" ,"iOS"),
-	WEB("WEB", "W", "WEB");
+	WEB("WEB", "W", "WEB"),
+	TIZEN("TIZEN", "T", "Tizen");
 
 	private final String code;
 	private final String shortCode;
