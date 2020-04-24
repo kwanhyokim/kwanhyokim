8c14ce434a24736a798032460bc722d1364b14ab
조민국/Music사업TF/SKP <skp.9261059@sk.com>
2020-04-24 15:01:33 +0900

    GODMUSIC-17057 청취 api 로그 -> 로그 파이프 라인 구축

---
+++ src/main/java/com/sktechx/godmusic/personal/rest/client/ExternalClient.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020 DREAMUS COMPANY.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of DREAMUS COMPANY.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with DREAMUS COMPANY.
+ */
+
+package com.sktechx.godmusic.personal.rest.client;
+
+import com.sktechx.godmusic.lib.domain.CommonApiResponse;
+import com.sktechx.godmusic.personal.rest.client.fallback.ExternalClientFallbackFactory;
+import com.sktechx.godmusic.personal.rest.model.vo.listen.ResourcePlayLogRequestParam;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * 설명 : XXXXXXXXXXX
+ *
+ * @author Groot(조민국) / dev.mingood@sk.com
+ * @since 2020. 04. 24
+ */
+@FeignClient(value = "external-api", fallbackFactory = ExternalClientFallbackFactory.class)
+public interface ExternalClient {
+
+    @GetMapping("/external/v1/ping")
+    CommonApiResponse<Void> ping();
+
+    @PostMapping("/external/v1/pipe/listen/log")
+    CommonApiResponse<?> sendListenLogRequest(@RequestBody ResourcePlayLogRequestParam requestParam);
+
+}
---
+++ src/main/java/com/sktechx/godmusic/personal/rest/client/fallback/ExternalClientFallbackFactory.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020 DREAMUS COMPANY.
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary information of DREAMUS COMPANY.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with DREAMUS COMPANY.
+ */
+
+package com.sktechx.godmusic.personal.rest.client.fallback;
+
+import com.sktechx.godmusic.lib.domain.CommonApiResponse;
+import com.sktechx.godmusic.personal.rest.client.ExternalClient;
+import com.sktechx.godmusic.personal.rest.model.vo.listen.ResourcePlayLogRequestParam;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 설명 : external 클라이언트 fallback
+ *
+ * @author Groot(조민국) / dev.mingood@sk.com
+ * @since 2020. 04. 24
+ */
+@Slf4j
+@Component
+public class ExternalClientFallbackFactory implements FallbackFactory<ExternalClient> {
+
+    @Override
+    public ExternalClient create(Throwable throwable) {
+        return new ExternalClient() {
+            @Override
+            public CommonApiResponse<Void> ping() {
+                log.warn("[WARM-UP] ... external Ping 호출 실패. message={}", throwable.getMessage());
+                return null;
+            }
+
+            @Override
+            public CommonApiResponse<?> sendListenLogRequest(ResourcePlayLogRequestParam requestParam) {
+                return null;
+            }
+        };
+    }
+
+}
