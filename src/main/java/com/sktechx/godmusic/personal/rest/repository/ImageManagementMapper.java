14631171d65fdfe6530e41db72671e9822d7bade
Bob <kwanhyo.kim@iriver.com>
2020-05-12 07:54:21 +0900

    [GODMUSIC-16815][server] 홈 개인화차트 디폴트 배경이미지 처리 : 캐쉬 정책 추가

--- src/main/java/com/sktechx/godmusic/personal/rest/repository/ImageManagementMapper.java
+++ src/main/java/com/sktechx/godmusic/personal/rest/repository/ImageManagementMapper.java
@@ -17,7 +17,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 import com.sktechx.godmusic.lib.domain.code.OsType;
-import com.sktechx.godmusic.lib.mybatis.annotation.BaseMapper;
+import com.sktechx.godmusic.lib.mybatis.annotation.ReadOnlyMapper;
 import com.sktechx.godmusic.lib.redis.annotation.RedisCacheable;
 import com.sktechx.godmusic.personal.common.domain.constant.RedisKeyConstant;
 import com.sktechx.godmusic.personal.rest.model.dto.ImageManagementDto;
@@ -29,20 +29,28 @@ import com.sktechx.godmusic.personal.rest.model.dto.chart.DispPropsImageDto;
  * @author 안영현/SKTECHX (younghyun.ahn@sk.com)
  * @date 2018. 9. 14.
  */
-@BaseMapper
+
+@ReadOnlyMapper
 public interface ImageManagementMapper {
 
-    @RedisCacheable(format = RedisKeyConstant.PERSONAL_IMAGE_MANAGEMENT_KEY, params = {"#p0" , "#p1" , "#p2"}, expireSeconds = 60)
+    @RedisCacheable(format = RedisKeyConstant.PERSONAL_IMAGE_MANAGEMENT_KEY,
+            params = {"#p0" , "#p1" , "#p2"}, expireSeconds = 60)
     List<ImageManagementDto> selectImageManagementList(@Param("imgContentType") String imgContentType,
                                                    @Param("imgContentId") Long imgContentId,
                                                    @Param("shortcutType") String shortcutType);
 
+    @RedisCacheable(format = RedisKeyConstant.PERSONAL_CHART_BACKGROUND_IMAGE_KEY,
+            params = { "#p0" , "#p1"})
     List<DispPropsImageDto> selectChartBackgroundImageList(@Param("dispId") Long dispId,
             @Param("osType") OsType osType
     );
 
+    @RedisCacheable(format = RedisKeyConstant.PERSONAL_PRICHART_DEFAULT_BACKGROUND_IMAGE_KEY,
+            params = { "#p0"})
     List<DispPropsImageDto> selectDefaultMixChartBackgroundImageList(@Param("dispId") Long dispId);
 
+    @RedisCacheable(format = RedisKeyConstant.PERSONAL_PRICHART_BACKGROUND_IMAGE_KEY,
+            params = { "#p0", "#p1"})
     List<DispPropsImageDto> selectMixChartBackgroundImageList(@Param("dispId") Long svcGenreId,
             @Param("osType") OsType osType);
 
