c68ced8d462f2ea3440c54859682aca7498cd24c
Bob <kwanhyo.kim@iriver.com>
2020-06-05 07:04:25 +0900

    [GODMUSIC-16386] 반응형 홈 패널 : 운영 TPO 버그 수정

--- src/main/java/com/sktechx/godmusic/personal/rest/service/recommend/panel/assembly/v2/OperationTpoPanelAssembly.java
+++ src/main/java/com/sktechx/godmusic/personal/rest/service/recommend/panel/assembly/v2/OperationTpoPanelAssembly.java
@@ -10,21 +10,25 @@
 
 package com.sktechx.godmusic.personal.rest.service.recommend.panel.assembly.v2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
+import com.sktechx.godmusic.lib.domain.CommonApiResponse;
 import com.sktechx.godmusic.lib.domain.code.OsType;
+import com.sktechx.godmusic.lib.domain.exception.CommonBusinessException;
+import com.sktechx.godmusic.lib.domain.exception.CommonErrorDomain;
 import com.sktechx.godmusic.personal.common.domain.type.RecommendPanelType;
+import com.sktechx.godmusic.personal.rest.client.DisplayClient;
 import com.sktechx.godmusic.personal.rest.model.dto.ChnlDto;
+import com.sktechx.godmusic.personal.rest.model.vo.ChannelListResponse;
 import com.sktechx.godmusic.personal.rest.model.vo.recommend.panel.Panel;
 import com.sktechx.godmusic.personal.rest.model.vo.recommend.panel.channel.TPOChannelPanel;
 import com.sktechx.godmusic.personal.rest.model.vo.recommend.panel.data.PanelContentVo;
 import com.sktechx.godmusic.personal.rest.model.vo.recommend.phase.PersonalPhaseMeta;
-import com.sktechx.godmusic.personal.rest.service.ChannelService;
 import com.sktechx.godmusic.personal.rest.service.recommend.panel.PanelNonSignAssembly;
 import lombok.extern.slf4j.Slf4j;
 
@@ -40,10 +44,10 @@ public class OperationTpoPanelAssembly extends PanelNonSignAssembly {
 
     private final int OPERATION_TPO_PANEL_HOME_MAX_SIZE = 7;
 
-    private final ChannelService channelService;
+    private final DisplayClient displayClient;
 
-    public OperationTpoPanelAssembly(ChannelService channelService){
-        this.channelService = channelService;
+    public OperationTpoPanelAssembly(DisplayClient displayClient){
+        this.displayClient = displayClient;
     }
 
     @Override
@@ -60,16 +64,32 @@ public class OperationTpoPanelAssembly extends PanelNonSignAssembly {
 
     private List<Panel> appendTPOPanel(final PersonalPhaseMeta personalPhaseMeta) {
         List<Panel> panelList = new ArrayList<>();
+        CommonApiResponse<ChannelListResponse> chnlDtoCommonApiResponse = displayClient.getOperationTpoList();
 
-        channelService.getOperationTpoChannelList()
-        .forEach(channel -> {
-            try {
-                panelList.add(createTPOChannelPanel(channel, personalPhaseMeta));
-            } catch (Exception e) {
-                log.error("TPO Panel defaultPanelSetting Exception : {}", e.getMessage());
-            }
-        });
+        Optional.ofNullable(chnlDtoCommonApiResponse)
+                .ifPresent(channelListResponseCommonApiResponse -> {
+                    if ("2000000".equals(chnlDtoCommonApiResponse.getCode())) {
+                        Optional.ofNullable(channelMapper.selectChannelByIds(Optional.ofNullable(
+                                Optional.ofNullable(channelListResponseCommonApiResponse.getData())
+                                        .orElseThrow(() -> new CommonBusinessException(
+                                                CommonErrorDomain.EMPTY_DATA)).getList())
+                                .orElseThrow(() -> new CommonBusinessException(CommonErrorDomain.EMPTY_DATA)).stream()
+                                .map(ChnlDto::getChnlId).collect(Collectors.toList())))
+                                .orElseThrow(() -> new CommonBusinessException(CommonErrorDomain.EMPTY_DATA)).stream()
+                                .filter(Objects::nonNull)
+                                .sorted(Comparator.comparing(ChnlDto::getCreateDtime).reversed())
+                                .collect(Collectors.toList())
+                                .forEach(channel -> {
+                                    try {
+                                        panelList.add(createTPOChannelPanel(channel,
+                                                personalPhaseMeta));
+                                    } catch (Exception e) {
+                                        log.error("TPO Panel defaultPanelSetting Exception : {}", e.getMessage());
+                                    }
+                                });
+                    }
 
+                });
         return panelList;
     }
 
