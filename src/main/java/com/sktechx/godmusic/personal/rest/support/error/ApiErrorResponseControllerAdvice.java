0a160edff24cfe9218f3e9edca5adfd910c30bcc
송대권(Daniel) <daekwon.song@sk.com>
2020-06-12 11:29:24 +0900

    [FLO-255] RestControllerAdvice 추가 및 좋아요 추가시 disp_sn update  오류 수정

---
+++ src/main/java/com/sktechx/godmusic/personal/rest/support/error/ApiErrorResponseControllerAdvice.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2020 DREAMUS COMPANY.
+ * All right reserved.
+ * This software is the confidential and proprietary information of DREAMUS COMPANY.
+ * You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with DREAMUS COMPANY.
+ */
+
+package com.sktechx.godmusic.personal.rest.support.error;
+
+import com.sktechx.godmusic.lib.domain.CommonApiResponse;
+import com.sktechx.godmusic.lib.domain.exception.CommonErrorDomain;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * 설명 :
+ *
+ * @author Daniel/DREAMUS COMPANY (daekwon.song@sk.com)
+ * @date 2020. 06. 11.
+ */
+@Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice
+public class ApiErrorResponseControllerAdvice {
+
+    @ExceptionHandler(value = {
+            MethodArgumentNotValidException.class,
+            MissingServletRequestParameterException.class
+    })
+    public ResponseEntity<CommonApiResponse<?>> handleException(Throwable ex, HttpServletRequest request, HttpServletResponse response) {
+
+        log.warn("[ApiErrorResponseControllerAdvice] 파라미터전달값오류 - {}", ex.getMessage());
+
+        CommonErrorDomain errorDomain = CommonErrorDomain.BAD_REQUEST;
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(CommonApiResponse.builder()
+                        .code(String.valueOf(errorDomain.getCode()))
+                        .message(errorDomain.getDisplayMessage())
+                        .traceId(MDC.get("transactionId"))
+                        .errorDomain(errorDomain)
+                        .build());
+    }
+
+    @SuppressWarnings("unchecked")
+    @ExceptionHandler(value = {
+            DuplicateKeyException.class
+    })
+    public ResponseEntity<CommonApiResponse<?>> handleSQLException(Throwable ex, HttpServletRequest request, HttpServletResponse response) {
+
+        log.warn("[ApiErrorResponseControllerAdvice] DB중복에러발생 - {}", ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(CommonApiResponse.emptySuccess());
+    }
+}
